e9405734a243368874cbb236e734e20dacc9080d
ibnuputraard <ibnu.putra@nawadata.id>
2024-05-27 17:59:30 +0700

    added kapal model

---
+++ models/kapal.model.js
@@ -0,0 +1,67 @@
+module.exports = (sequelize, Sequelize) => {
+  const KAPAL = sequelize.define(
+    "t_kapal",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nama_kapal: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      nama_agen: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      kapten_kapal: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      gross_tone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      bendera: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      imo: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      negara_asal: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      tipe_kapal: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      tanggal_permintaan: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      tanggal_diperiksa: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      tanggal_disetujui: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "t_kapal",
+      timestamps: false,
+      underscore: true,
+    }
+  );
+
+  return KAPAL;
+};
